e72aa8a2576ea6bc516b42cda174c65303c3373d
pierre-stephaneokyere <pierre.okyere@hotmail.co.uk>
2021-11-04 15:06:19 +0000

    Scheduling scripts to run every 24 hours

--- test_table_to_sheets.py
+++ test_table_to_sheets.py
@@ -1,7 +1,8 @@
 # this file shows how to use and call the table_to_sheets function
 # you can also just run the python script to test the code and the set up as below
 # python3 postgres_to_google_sheets
-
+import schedule as schedule
+import time
 from postgres_to_google_sheets import table_to_sheets
 from config import config
 
@@ -11,4 +12,8 @@ table_name = config_data.get('table_name')
 sheet_name = config_data['sheet_name']
 
 # call function to transfer data from a table to google sheets
-table_to_sheets(table_name, sheet_name)
+
+schedule.every(24).hours.do(table_to_sheets,table_name, sheet_name)
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
--- truelayer.py
+++ truelayer.py
@@ -1,63 +1,69 @@
+import schedule as schedule
+import time
 from kaggle.api.kaggle_api_extended import KaggleApi
 import zipfile
 import pandas as pd
 from sqlalchemy import create_engine
-import json
-import ast
-import gzip
-import requests
-import urllib.request
 import wikipediaapi
 
-api = KaggleApi()
-api.authenticate()
-#
-# #Downloading Kaggle File
-url = 'https://www.kaggle.com/rounakbanik/the-movies-dataset/version/7#movies_metadata.csv'
-#
-# api.dataset_download_files('rounakbanik/the-movies-dataset','/Users/pierre-stephaneokyere/Documents/TrueLayer',unzip= True)
-api.dataset_download_file('rounakbanik/the-movies-dataset/version/','movies_metadata.csv')
+def pushdata():
+    api = KaggleApi()
+    api.authenticate()
+    #
+    # #Downloading Kaggle File
+    url = 'https://www.kaggle.com/rounakbanik/the-movies-dataset/version/7#movies_metadata.csv'
+    #
+    # api.dataset_download_files('rounakbanik/the-movies-dataset','/Users/pierre-stephaneokyere/Documents/TrueLayer',unzip= True)
+    api.dataset_download_file('rounakbanik/the-movies-dataset/version/','movies_metadata.csv')
 
-zf = zipfile.ZipFile('movies_metadata.csv.zip')
-zf.extractall()
-zf.close()
+    zf = zipfile.ZipFile('movies_metadata.csv.zip')
+    zf.extractall()
+    zf.close()
 
-df = pd.read_csv('movies_metadata.csv',low_memory=False)
+    df = pd.read_csv('movies_metadata.csv',low_memory=False)
 
-df['budget']=pd.to_numeric(df['budget'],errors='coerce').fillna(0)
-df['revenue']=pd.to_numeric(df['revenue'],errors='coerce').fillna(0)
-df['release_date']=df['release_date'].str[:4].astype(float)
-df['budget']= df['budget'].replace(0,1)
-df['revenue']= df['revenue'].replace(0,1)
-df2 = df.assign(ratio=df['budget']/df['revenue'])
+    df['budget']=pd.to_numeric(df['budget'],errors='coerce').fillna(0)
+    df['revenue']=pd.to_numeric(df['revenue'],errors='coerce').fillna(0)
+    df['release_date']=df['release_date'].str[:4].astype(float)
+    df['budget']= df['budget'].replace(0,1)
+    df['revenue']= df['revenue'].replace(0,1)
+    df2 = df.assign(ratio=df['budget']/df['revenue'])
 
-df_tfr = df2.nlargest(1000,'ratio',keep='first')
+    df_tfr = df2.nlargest(1000,'ratio',keep='first')
 
-df_tfr = df_tfr[['original_title','revenue','budget','release_date','vote_average','ratio','production_companies']]
+    df_tfr = df_tfr[['original_title','revenue','budget','release_date','vote_average','ratio','production_companies']]
 
-wiki_wiki = wikipediaapi.Wikipedia('en')
+    wiki_wiki = wikipediaapi.Wikipedia('en')
 
-rows_url = []
-rows_summary = []
-for i in range(len(df_tfr['original_title'])):
-    try:
-        wikititle = df_tfr['original_title'][i]
-        page_url = wiki_wiki.page(wikititle).fullurl
-        page_summary = wiki_wiki.page(wikititle).summary
-    except:
-        page_url = ''
-        page_summary=''
-    rows_url.append(page_url)
-    rows_summary.append(page_summary)
-df3 = pd.DataFrame(rows_url)
-df4 = pd.DataFrame(rows_summary)
+    rows_url = []
+    rows_summary = []
+    for i in range(len(df_tfr['original_title'])):
+        try:
+            wikititle = df_tfr['original_title'][i]
+            page_url = wiki_wiki.page(wikititle).fullurl
+            page_summary = wiki_wiki.page(wikititle).summary
+        except:
+            page_url = ''
+            page_summary=''
+        rows_url.append(page_url)
+        rows_summary.append(page_summary)
+    df3 = pd.DataFrame(rows_url)
+    df4 = pd.DataFrame(rows_summary)
 
-df_tfr= df_tfr.assign(url=df3)
-df_tfr= df_tfr.assign(summary=df4)
-print(df_tfr)
+    df_tfr= df_tfr.assign(url=df3)
+    df_tfr= df_tfr.assign(summary=df4)
 
-engine = create_engine('postgresql+psycopg2://postgres:dover2210@localhost:9011/truefilm', pool_pre_ping=True)
-df_tfr.to_sql(name = 'truefilm_tfr', con = engine, if_exists='replace', index = False)
+    print(df_tfr)
+
+    engine = create_engine('postgresql+psycopg2://postgres:dover2210@localhost:9011/truefilm', pool_pre_ping=True)
+    df_tfr.to_sql(name = 'truefilm_tfr', con = engine, if_exists='replace', index = False)
+
+schedule.every(24).hours.do(pushdata)
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
+
+
 
 
 
